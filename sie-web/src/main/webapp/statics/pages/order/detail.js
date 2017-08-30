600a5ebba7d8ecf7faea1d6145fd0db119f3bfbc
wangheng19900315 <wangheng19900315@163.com>
2017-08-30 23:38:24 +0800

    hello

--- sie-web/src/main/webapp/statics/pages/order/detail.js
+++ sie-web/src/main/webapp/statics/pages/order/detail.js
@@ -43,19 +43,20 @@ $(function(){
         //height: '100%',
         mtype: 'post',
         postData: {},
-        colNames: ['ID', '项目名','创建时间','课程数'  ,'课程名' ,'金额' ,'状态'],
+        colNames: ['ID','projectId','courseIds', '项目名','创建时间','住宿名称','课程数'  ,'课程名'],
         colModel: [
             {name: 'id', index: 'id', width: 20, hidden: true, sorttype: "int", sortable: false},
+            {name: 'projectId', index: 'projectId', width: 20, hidden: true,   sortable: false},
+            {name: 'courseIds', index: 'courseIds', width: 20, hidden: true,  sortable: false},
             {name: 'projectName', index: 'projectName', width: 120, sortable: false},
-            {name: 'createTime', index: 'createTime', width: 160 , sortable: false, formatter:function(cellvalue, options, rowObject){
+            {name: 'createTime', index: 'createTime', width: 120 , sortable: false, formatter:function(cellvalue, options, rowObject){
                 var time1 = new Date(cellvalue).Format("yyyy-MM-dd hh:mm:ss");
                 return time1;
             }
             },
-            {name: 'courseCount', index: 'courseCount', width: 120, sortable: false},
-            {name: 'custerNames', index: 'custerNames', width: 120, sortable: false},
-            {name: 'total', index: 'total', width: 120, sortable: false},
-            {name: 'orderDetailStatusName', index: 'orderDetailStatusName', width: 120, sortable: false}
+            {name: 'dormitoryName', index: 'dormitoryName', width: 120, sortable: false},
+            {name: 'courseCount', index: 'courseCount', width: 60, sortable: false},
+            {name: 'custerNames', index: 'custerNames', width: 360, sortable: false}
         ],
         multiselect: true,
         multiboxonly: true,
