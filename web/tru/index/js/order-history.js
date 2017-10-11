30788195529c6df4e4dff30014b76b6c48f97958
wangheng19900315 <wangheng19900315@163.com>
2017-10-11 20:40:11 +0800

    order history

--- web/tru/index/js/order-history.js
+++ web/tru/index/js/order-history.js
@@ -1,5 +1,6 @@
 var termOrder;
 $(function(){
+
 	/**
 	 * 加载订单信息
 	 */
@@ -11,7 +12,7 @@ $(function(){
 	 */
 	dhcc.Unit.ajaxUtil(attrs,"getOrderList.json",function(data){
 		termOrder = data;
-		var term = $("ul[aria-labelledby='term']");
+		var term = $("#term").next();
 		var termLi = '';
 		//初始化学期
 		$.each(termOrder,function(key,item){
@@ -20,39 +21,65 @@ $(function(){
 		});
 		term.empty();
 		term.append(termLi);
-		//var orders = '<tr> <th>订单编号</th>' +
-		//			'<th>状态</th>' +
-		//			'<th>支付方式</th>' +
-		//			'<th>项目</th>' +
-		//			'<th>课程数目</th>' +
-		//			'<th>住宿</th>' +
-		//			'<th>总价</th> </tr>';
-		//var courses = '<tr> <th>课程代码</th>' +
-		//				'<th>课程名称</th>' +
-		//				'<th>校区</th> </tr>';
-		//$.each(data,function(i,order){
-		//	orders = orders +
-		//		'<tr><td><label>'+ order.code + '</label></td>'+
-		//		'<td><label>' + order.statusName + '</label></td>'+
-		//		'<td><label>' + order.payTypeName + '</label></td>'+
-		//		'<td><label>' + order.projectNames + '</label></td>'+
-		//		'<td><label>' + order.courseNumber + '</label></td>'+
-		//		'<td><label>' + order.dormitoryNames + '</label></td>'+
-		//		'<td><label>' + order.payMoney + '</label></td></tr>';
-		//	$.each(order.courses,function(ii,course){
-		//		courses = courses +
-		//				'<tr> <td><label>' + course.courseID + '</label></td>' +
-		//				'<td><label>' + course.truCode + '</label></td>' +
-		//				'<td><label>' +course.schoolName + '</label></td> </tr>';
-		//	});
-        //
-		//});
-		//var orderTable = $($(".table")[0]);
-		//orderTable.empty();
-		//orderTable.append(orders);
-        //
-		//var courseTable = $($(".table")[1]);
-		//courseTable.empty();
-		//courseTable.append(courses);
+		initSelect();
+
+		//默认选中第一个学期
+		if(termLi != ''){
+			term.find("li").click();
+		}
 	});
 })
+
+//初始化select方法
+function initSelect(onChange){
+	$(".dropdown-select li").click(function() {
+		if ($(this).attr("class") !="disabled") {
+			var txt = $(this).text();
+			var oldText = $(this).parent().prev().val();
+			$(this).parent().prev().val(txt);
+			var cl = $(this).attr("aria-id");
+			$("#" + cl).show().siblings().hide();
+			if(txt != oldText){
+				changeTerm(txt);
+			}
+		}
+	});
+}
+
+function changeTerm(term){
+	var termOrders = termOrder[term];
+	var orders = '<tr> <th>订单编号</th>' +
+				'<th>状态</th>' +
+				'<th>支付方式</th>' +
+				'<th>项目</th>' +
+				'<th>课程数目</th>' +
+				'<th>住宿</th>' +
+				'<th>总价</th> </tr>';
+	var courses = '<tr> <th>课程代码</th>' +
+					'<th>课程名称</th>' +
+					'<th>校区</th> </tr>';
+	$.each(termOrders,function(i,order){
+		orders = orders +
+			'<tr><td><label>'+ order.code + '</label></td>'+
+			'<td><label>' + order.statusName + '</label></td>'+
+			'<td><label>' + order.payTypeName + '</label></td>'+
+			'<td><label>' + order.projectNames + '</label></td>'+
+			'<td><label>' + order.courseNumber + '</label></td>'+
+			'<td><label>' + order.dormitoryNames + '</label></td>'+
+			'<td><label>' + order.payMoney + '</label></td></tr>';
+		$.each(order.courses,function(ii,course){
+			courses = courses +
+					'<tr> <td><label>' + course.courseID + '</label></td>' +
+					'<td><label>' + course.truCode + '</label></td>' +
+					'<td><label>' +course.schoolName + '</label></td> </tr>';
+		});
+
+	});
+	var orderTable = $($(".table")[0]);
+	orderTable.empty();
+	orderTable.append(orders);
+
+	var courseTable = $($(".table")[1]);
+	courseTable.empty();
+	courseTable.append(courses);
+}
