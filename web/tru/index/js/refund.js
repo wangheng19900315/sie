2cb2d4c96054a77e34a8189fd1e9990dc95f0ddd
wangheng <wangheng5@jd.com>
2017-10-22 16:19:02 +0800

    moidfy payment html

--- web/tru/index/js/refund.js
+++ web/tru/index/js/refund.js
@@ -51,7 +51,7 @@ $(function(){
 		dhcc.Unit.ajaxUtil(params,"refundOrder.json",function(data){
 			dhcc.Unit.successMessage("提交成功",function(){
 				//页面进行跳转
-				//window.location.href = "project-registration.html";
+				window.location.href = "user-center.html";
 			});
 		});
 	});
